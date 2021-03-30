6bb9bc9ba8a436cd96d1ac81920e1fe1e45d9fc0
Pierre Luycx <pierre.luycx@gmail.com>
2021-03-30 11:28:10 +0200

    Complete home page

--- open_data/app/urls.py
+++ open_data/app/urls.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('quiz', TemplateView.as_view(template_name='quiz.html'), name='quiz'),
+    path('forum', TemplateView.as_view(template_name='forum.html'), name='forum'),
     path('profile', TemplateView.as_view(template_name='profile.html'), name='profile'),
 ]
