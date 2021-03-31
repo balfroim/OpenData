5a67cbe7629b5a5ede5fb5b7c27fe9de2c7de6bb
Martin <martin.balfroid@student.unamur.be>
2021-03-31 16:26:12 +0200

    Add query to get the most popular datasets with icons

--- open_data/app/urls.py
+++ open_data/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import homepage
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', homepage, name='home'),#TemplateView.as_view(template_name='home.html'), name='home'),
     path('quiz', TemplateView.as_view(template_name='quiz.html'), name='quiz'),
     path('forum', TemplateView.as_view(template_name='forum.html'), name='forum'),
     path('profile', TemplateView.as_view(template_name='profile.html'), name='profile'),
