a0aa6ce62add6b793d6da75e1213a5d3dd82a214
Martin <martin.balfroid@student.unamur.be>
2021-04-04 09:58:09 +0200

    Add svgimagefield from https://7webpages.com/blog/how-to-have-svg-allowed-as-an-image-for-django-rest-framework/

--- open_data/dataset/models.py
+++ open_data/dataset/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 
-# class Theme(models.Model):
-#     name = models.CharField(max_length=100, default='')
-#     svg_file_name = models.CharField(max_length=50, default='')
+class Theme(models.Model):
+    name = models.CharField(max_length=100, default='')
+    svg_file_name = models.CharField(max_length=50, default='', null=True)
 
 
-class ProxyDataset(models.Model):
-    # slug = models.CharField(max_length=100, default='')
-    # title = models.CharField(max_length=255, default='')
-    # theme = models.ForeignKey(Theme, on_delete=models.SET_NULL, related_name='datasets', null=True)
-    json_dump = models.JSONField()
+# class ProxyDataset(models.Model):
+#     # slug = models.CharField(max_length=100, default='')
+#     # title = models.CharField(max_length=255, default='')
+#     # theme = models.ForeignKey(Theme, on_delete=models.SET_NULL, related_name='datasets', null=True)
+#     json_dump = models.JSONField()
