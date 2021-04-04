3c088ff4111493450677e78f2f0888329576c322
Pierre Luycx <pierre.luycx@gmail.com>
2021-04-04 10:58:39 +0200

    Modal login (first draft)

--- open_data/app/urls.py
+++ open_data/app/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
 
+from .views import home, signin
+
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', home, name='home'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('signin/', signin, name='signin'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('quiz/', TemplateView.as_view(template_name='quiz.html'), name='quiz'),
     path('forum/', TemplateView.as_view(template_name='forum.html'), name='forum'),
