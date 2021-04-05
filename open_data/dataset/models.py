6a3e87719b19068ba509f1351698f306d36df9f7
Martin <martin.balfroid@student.unamur.be>
2021-04-05 10:56:34 +0200

    Add slug field to Theme

--- open_data/dataset/models.py
+++ open_data/dataset/models.py
@@ -1,13 +1,22 @@
 from dataset.svg import validate_svg
 from django.db import models
+from django.utils.text import slugify
 
 
 class Theme(models.Model):
-    name = models.CharField(max_length=100, default='')
-    image = models.FileField(upload_to ='uploads/themes/', validators=[validate_svg], null=True, blank=True)
+    name = models.CharField(max_length=100, default='', unique=True)
+    slug = models.CharField(max_length=100, default='')
+    image = models.FileField(upload_to='themes/', validators=[validate_svg], null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        return super(Theme, self).save(*args, **kwargs)
 
-# class ProxyDataset(models.Model):
+    def __str__(self):
+        return f"{self.name} ({self.image})"
+
+        # class ProxyDataset(models.Model):
 #     pass
 #     # slug = models.CharField(max_length=100, default='')
 #     # title = models.CharField(max_length=255, default='')
