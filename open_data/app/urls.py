e7ea922ff6c41887a2d30cfb4e5b508a60d08a41
Martin <martin.balfroid@student.unamur.be>
2021-04-05 11:17:30 +0200

    ThemeAdmin

--- open_data/app/urls.py
+++ open_data/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from django.views.generic import TemplateView
 from . import views
+from open_data.settings import MEDIA_URL, MEDIA_ROOT
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,4 +11,4 @@ urlpatterns = [
     path('quizzes', views.quizzes, name='quizzes'),
     path('forum', TemplateView.as_view(template_name='forum.html'), name='forum'),
     path('profile', TemplateView.as_view(template_name='profile.html'), name='profile'),
-]
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
--- open_data/dataset/admin.py
+++ open_data/dataset/admin.py
@@ -1,4 +1,11 @@
 from dataset.models import Theme
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-admin.site.register(Theme)
+
+@admin.register(Theme)
+class HeroAdmin(admin.ModelAdmin):
+    readonly_fields = ["name", "slug", "preview_image"]
+
+    def preview_image(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" width=128px height=128px/>')
