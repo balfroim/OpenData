dceb46c8171f6eaaaebf9e25839abb45cbd85236
Martin <martin.balfroid@student.unamur.be>
2021-04-07 12:32:12 +0200

    Add nested inlines for quizz creation in admin

--- open_data/quiz/admin.py
+++ open_data/quiz/admin.py
@@ -1,14 +1,20 @@
 from .models import Quiz, Question, Answer
 from django.contrib import admin
+from nested_inline import admin as nested_admin
 
-class QuestionInline(admin.StackedInline):
+class AnswerInline (nested_admin.NestedTabularInline):
+    model = Answer
+    extra = 1
+
+class QuestionInline(nested_admin.NestedStackedInline):
     model = Question
-
-
-
+    inlines = [
+        AnswerInline
+    ]
+    extra = 1
 
 @admin.register(Quiz)
-class QuizAdmin(admin.ModelAdmin):
+class QuizAdmin(nested_admin.NestedModelAdmin):
     inlines = [
-        QuestionInline,
+        QuestionInline
     ]
