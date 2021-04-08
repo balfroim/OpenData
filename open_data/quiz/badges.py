0b753edb6bc37ac0dd438c9c7fa08ee7fece9699
Martin <martin.balfroid@student.unamur.be>
2021-04-08 18:43:18 +0200

    Badge award pour les quizz

---
+++ open_data/quiz/badges.py
@@ -0,0 +1,28 @@
+from pinax.badges.base import Badge, BadgeAwarded
+
+
+class QuizBadge(Badge):
+    slug = "quiz"
+    levels = [
+        "Bronze",
+        "Silver",
+        "Gold",
+    ]
+    events = [
+        "quiz_submit",
+    ]
+    multiple = False
+
+    def award(self, **state):
+        user = state["user"]
+        quiz_submission_count = user.quizzes_taken.count()
+        award = None
+        if quiz_submission_count >= 3:
+            award = BadgeAwarded(level=3)
+        elif quiz_submission_count >= 2:
+            award = BadgeAwarded(level=2)
+        elif quiz_submission_count >= 1:
+            award = BadgeAwarded(level=1)
+        return award
+
+
