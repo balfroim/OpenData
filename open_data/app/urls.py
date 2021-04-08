fdd7a82923f8f7ffb13de7bc1754e3098dcdc0d7
Pierre Luycx <pierre.luycx@gmail.com>
2021-04-08 10:07:12 +0200

    Reorganize urls

--- open_data/app/urls.py
+++ open_data/app/urls.py
@@ -1,16 +1,13 @@
-from django.urls import include, path
-from django.conf.urls.static import static
+from django.urls import path
 from django.views.generic import TemplateView
-from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 from open_data import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('quiz/', include('quiz.urls'), name='quiz'),
     path('quizzes/', views.quizzes, name='quizzes'),
     path('forum/', TemplateView.as_view(template_name='forum.html'), name='forum'),
     path('profile/', TemplateView.as_view(template_name='profile.html'), name='profile'),
     path('search/', views.search, name='search'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- open_data/quiz/urls.py
+++ open_data/quiz/urls.py
@@ -2,14 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = "quiz"
 urlpatterns = [
-    # ex: /quiz/
     path('', views.quizzes, name='list'),
-    # ex: /quiz/5/result
-    path('<int:quiz_id>/result/', views.result, name='result'),
-    # # ex: /polls/5/results/
-    # path('<int:quiz_id>/results/', views.results, name='results'),
-    # # ex: /polls/5/vote/
-    # path('<int:quiz_id>/vote/', views.vote, name='vote'),
+    path('<int:quiz_id>/', views.result, name='result'),
 ]
