66ded4a65a8aec8317b859079539031880962e5f
Pierre Luycx <pierre.luycx@gmail.com>
2021-04-08 17:25:40 +0200

    Add redirection to the right page after login

--- open_data/user/templatetags/auth.py
+++ open_data/user/templatetags/auth.py
@@ -4,11 +4,15 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 register = template.Library()
 
 
-@register.inclusion_tag('log-in-form.html')
-def log_in_form(form=AuthenticationForm()):
-    return {'form': form}
+@register.inclusion_tag('log-in-form.html', takes_context=True)
+def log_in_form(context, form=AuthenticationForm()):
+    request = context['request']
+    next = form.data.get('next', request.path)
+    return {'form': form, 'next': next}
 
 
-@register.inclusion_tag('sign-in-form.html')
-def sign_in_form(form=UserCreationForm()):
-    return {'form': form}
+@register.inclusion_tag('sign-in-form.html', takes_context=True)
+def sign_in_form(context, form=UserCreationForm()):
+    request = context['request']
+    next = form.data.get('next', request.path)
+    return {'form': form, 'next': next}
