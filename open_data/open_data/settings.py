b82c15c25ce91763fa94e4bf1625a64d2c9ba20e
Martin <martin.balfroid@student.unamur.be>
2021-04-09 14:44:44 +0200

    Import django notifications hq

--- open_data/open_data/settings.py
+++ open_data/open_data/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'colorfield',
     'nested_inline',
-    'pinax.badges'
+    'pinax.badges',
+    'notifications',
 ]
 
 MIDDLEWARE = [
--- open_data/open_data/urls.py
+++ open_data/open_data/urls.py
@@ -13,16 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import notifications.urls
+import pinax.badges.urls
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
 
 from .settings import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
-    path('', include('app.urls')),
-    path('auth/', include('user.urls')),
-    path('quiz/', include('quiz.urls')),
-    path('badges/', include("pinax.badges.urls", namespace="pinax_badges")),
-    path('admin/', admin.site.urls),
-] + static(MEDIA_URL, document_root=MEDIA_ROOT)
+                  path('', include('app.urls')),
+                  path('auth/', include('user.urls')),
+                  path('quiz/', include('quiz.urls')),
+                  path('badges/', include(pinax.badges.urls, namespace="pinax_badges")),
+                  path('inbox/notifications/', include(notifications.urls, namespace='notifications')),
+                  path('admin/', admin.site.urls),
+              ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
