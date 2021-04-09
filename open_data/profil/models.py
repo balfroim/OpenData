057e59bf2048b589a39cd39e01de215f0194f139
Hincql <47609259+Hincql@users.noreply.github.com>
2021-04-09 17:24:33 +0200

    update profil
    ajout facebooklink
    
    Co-Authored-By: M. Balfroid <36892842+balfroim@users.noreply.github.com>

--- open_data/profil/models.py
+++ open_data/profil/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Profil(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profil")
     description = models.TextField(max_length=200)
+    facebooklink = models.CharField(max_length=100, default ="")
 
     def __str__(self):
         return self.user.username
--- open_data/profil/views.py
+++ open_data/profil/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 
 
-def index(request):
-    return render(request, 'profil/profile.html')
-    #return HttpResponse("Hello, world. You're at the polls index.")
+def index(request,username):
+    user = get_object_or_404(User, username=username)
+    return render(request, 'profil/profile.html', {"targetuser": user})
