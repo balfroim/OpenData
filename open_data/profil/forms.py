37524d66e6125832b952f1be721e33fc27c33ec5
Hincql <47609259+Hincql@users.noreply.github.com>
2021-04-09 16:40:04 +0200

    correction profil
    correction

--- open_data/profil/forms.py
+++
@@ -1,3 +0,0 @@
-import itertools
-
-from django import forms
--- open_data/profil/views.py
+++ open_data/profil/views.py
@@ -4,7 +4,4 @@ from django.shortcuts import render, get_object_or_404
 
 def index(request):
     return render(request, 'profil/profile.html')
-
-
-
-#return HttpResponse("Hello, world. You're at the polls index.")
+    #return HttpResponse("Hello, world. You're at the polls index.")
