25e1586dfd0e7c3da084bed53c2a5e09822b2c85
Martin <martin.balfroid@student.unamur.be>
2021-04-10 14:37:06 +0200

    No link

--- open_data/profil/models.py
+++ open_data/profil/models.py
@@ -3,9 +3,7 @@ from django.contrib.auth.models import User
 
 class Profil(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profil", null=True)
-    created_at = models.DateTimeField(editable=False)
     description = models.TextField(max_length=200)
-    facebooklink = models.CharField(max_length=100, default ="")
 
     def __str__(self):
         return self.user.username
