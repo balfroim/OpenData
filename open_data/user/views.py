ef4d3cacf381d85d28ea2bd9461f36d95794c786
Pierre Luycx <pierre.luycx@gmail.com>
2021-04-12 17:18:11 +0200

    Fix sign in route

--- open_data/user/views.py
+++ open_data/user/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
+from profil.models import Profil
+
 
 def sign_in(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            profile = Profil.objects.create(user=user)
+            profile.save()
             login(request, user)
-            return redirect('profile')
+            return redirect('profil', username=user.username)
     else:
         form = UserCreationForm()
 
