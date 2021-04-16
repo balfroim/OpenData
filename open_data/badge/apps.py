527bc8eb4a68a931e19aee5294192276366dff7c
Martin <martin.balfroid@student.unamur.be>
2021-04-16 17:23:02 +0200

    filter not subclasses

--- open_data/badge/apps.py
+++ open_data/badge/apps.py
@@ -1,7 +1,8 @@
-import inspect
 import importlib
-from importlib import import_module
+import inspect
+
 from django.apps import AppConfig
+
 from open_data.settings import INSTALLED_APPS
 
 
@@ -11,14 +12,14 @@ class BadgeConfig(AppConfig):
     def ready(self):
         from badge.base import Badge
         from badge.registry import BadgeCache
+        # Import dynamically all the Badge subclasses in the installed apps.
         for module_name in INSTALLED_APPS:
             try:
                 module = importlib.import_module(f"{module_name}.badges", package=None)
             except ModuleNotFoundError:
                 pass
             else:
-                classes = [cls for name, cls in inspect.getmembers(module, inspect.isclass) if
-                           name.endswith("Badge") and name != 'Badge']
+                classes = [cls for name, cls in inspect.getmembers(module, inspect.isclass) if Badge in cls.__bases__]
                 for cls in classes:
                     BadgeCache.instance().register(cls)
-        from badge.signals import badge_awarded
+        badge_awarded = getattr(importlib.import_module("badge.signals"), "badge_awarded")
