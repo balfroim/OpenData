7b07201995eb000105399c93001aa54164de137f
Maxime Caucheteur <maxcauch@hotmail.com>
2021-04-17 16:27:05 +0200

    Modif url

--- open_data/theme/urls.py
+++ open_data/theme/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<theme_name>/', views.theme, name='theme'),
+    path('<str:theme_name>/', views.theme, name='theme'),
 ]
