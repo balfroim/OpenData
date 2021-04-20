a1c4d1944b7e8c13a9788f6d2bf81966f8b74469
Pierre Luycx <pierre.luycx@gmail.com>
2021-04-20 17:59:58 +0200

    Transform like button into an AJAX request

--- open_data/dataset/views.py
+++ open_data/dataset/views.py
@@ -1,7 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from badge.registry import BadgeCache
+
 from .models import Theme, ProxyDataset
+from badge.registry import BadgeCache
 
 
 def theme_page(request, theme_id):
@@ -17,9 +19,12 @@ def dataset_page(request, dataset_id):
 @require_POST
 def toggle_like(request, dataset_id):
     dataset = get_object_or_404(ProxyDataset, id=dataset_id)
-    if dataset in request.user.profile.liked_datasets.all():
-        request.user.profile.liked_datasets.remove(dataset)
-    else:
+    liked = dataset not in request.user.profile.liked_datasets.all()
+    if liked:
         request.user.profile.liked_datasets.add(dataset)
-    BadgeCache.instance().possibly_award_badge("on_dataset_liked", user=request.user)
-    return redirect('dataset', dataset_id=dataset.id)
+    else:
+        request.user.profile.liked_datasets.remove(dataset)
+
+    BadgeCache.instance().possibly_award_badge('on_dataset_liked', user=request.user)
+
+    return JsonResponse({'liked': liked, 'n_likes': dataset.liking_users.count()})
