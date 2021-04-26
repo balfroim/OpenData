a7b8d3ea8960ecc1836092610b1438edf03ae5b7
Pierre Luycx <pierre.luycx@gmail.com>
2021-04-26 11:50:17 +0200

    Remove unused urls

--- open_data/app/urls.py
+++ open_data/app/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('forum/', TemplateView.as_view(template_name='forum.html'), name='forum'),
     path('scoreboard/', views.scoreboard, name='scores'),
-    path('data/', TemplateView.as_view(template_name='dataset_theme.html'), name='data'),
 ]
