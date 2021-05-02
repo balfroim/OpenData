c012a33d81e57f07201a4dad5b9d656495dadb91
Hincql <47609259+Hincql@users.noreply.github.com>
2021-05-02 12:08:11 +0200

    Add new badges

--- open_data/dataset/badges.py
+++ open_data/dataset/badges.py
@@ -1,24 +1,21 @@
+from abc import ABCMeta
+
 from badge.base import Badge, BadgeDetail, BadgeAwarded
+from badge.stereotypes import ThresholdedBadge, OnceBadge
 
 
-def check_threshold(levels, thresholds, observed_value):
-    award = None
-    for lvl in range(len(levels)):
-        if observed_value >= thresholds[lvl]:
-            award = BadgeAwarded(level=lvl+1)
-    return award
-
-
-class LikedDatasetBadge(Badge):
-    slug = "dataset-like"
+class LikeDatasetBadge(ThresholdedBadge):
+    slug = 'dataset-like'
     levels = [
         BadgeDetail(
             name="J'aime donc je suis",
-            description="Liker un dataset."
+            description='Liker un jeu de données.',
+            score=10
         ),
         BadgeDetail(
-            name="Open relationship",
-            description="Liker cinq datasets."
+            name='Open relationship',
+            description='Liker cinq jeux de données.',
+            score=50
         ),
     ]
     level_thresholds = [
@@ -26,11 +23,205 @@ class LikedDatasetBadge(Badge):
         5,
     ]
     events = [
-        "on_dataset_like",
+        'on_dataset_like',
+    ]
+
+    def thresholded_value(self, user):
+        return user.profile.liked_datasets.count()
+
+
+class QuizDatasetInspection(OnceBadge):
+    slug = 'quiz-dataset-inspection'
+    levels = [
+        BadgeDetail(
+            name='La curiosité est un bon défaut',
+            description='Inspecter le jeu de données lié à un quiz.',
+            score=20
+        ),
+    ]
+    events = [
+        'on_linked_quiz_inspect',
+    ]
+
+
+class DownloadDatasetBadge(OnceBadge):
+    slug = 'dataset-download'
+    levels = [
+        BadgeDetail(
+            name='Données ouvertes',
+            description='Télécharger un jeu de données.',
+            score=80
+        ),
+    ]
+    events = [
+        'on_dataset_download',
+    ]
+
+
+class FirstCommentDatasetBadge(Badge, metaclass=ABCMeta):
+    slug = 'dataset-first-comment'
+    levels = [
+        BadgeDetail(
+            name='First !',
+            description='Être le premier à commenter un jeu de données.',
+            score=200
+        ),
+    ]
+    events = [
+        'on_question_ask',
     ]
     multiple = False
 
-    def award(self, **state):
-        user = state["user"]
-        nb_liked_dataset = len(user.profile.liked_datasets.all())
-        return check_threshold(self.levels, self.level_thresholds, nb_liked_dataset)
+    def award(self, **states):
+        dataset = states['dataset']
+        if dataset.questions.count() == 1:
+            return BadgeAwarded(level=1)
+        return None
+
+
+class SubscribeThemeBadge(ThresholdedBadge):
+    slug = 'theme-subscribe'
+    levels = [
+        BadgeDetail(
+            name="Intéressant ?",
+            description='S\'abonner à un thème.',
+            score=10
+        ),
+        BadgeDetail(
+            name='Stay tuned',
+            description='S\'abonner à cinq thèmes.',
+            score=50
+        ),
+        BadgeDetail(
+            name='Rule them all',
+            description='S\'abonner à dix thèmes.',
+            score=50
+        ),
+    ]
+    level_thresholds = [
+        1,
+        5,
+        10,
+    ]
+    events = [
+        'on_theme_subscription',
+    ]
+
+    def thresholded_value(self, user):
+            return user.profile.theme_subscriptions.count()
+
+
+class ExploreDatasetBadge(ThresholdedBadge):
+    slug = 'dataset-explore'
+    levels = [
+        BadgeDetail(
+            name="Touriste",
+            description='Explorer 1 datasets.',
+            score=10
+        ),
+        BadgeDetail(
+            name='Explorateur',
+            description='Explorer 5 datasets.',
+            score=40
+        ),
+        BadgeDetail(
+             name='Globetrotter',
+             description='Explorer 10 datasets.',
+             score=50
+        ),
+    ]
+    level_thresholds = [
+        1,
+        5,
+        10,
+    ]
+    events = [
+        'on_dataset_explore',
+    ]
+
+    def thresholded_value(self, user):
+        #TODO
+        return 0
+
+
+class VisitedAllThemeDatasetBadge(Badge, metaclass=ABCMeta):
+    slug = 'dataset-first-comment'
+    levels = [
+        BadgeDetail(
+            name='Le tour de la question',
+            description='Explorer au moins un dataset par thème.',
+            score=100
+        ),
+    ]
+    events = [
+        'on_dataset_explore',
+    ]
+
+
+class AskQuestionBadge(ThresholdedBadge):
+    slug = 'question-ask'
+    levels = [
+        BadgeDetail(
+            name='Il n\'y a pas de question bête',
+            description='Poser une question',
+            score=20
+        ),
+        BadgeDetail(
+            name='non sérieusement continue à poser des questions',
+            description='Poser 20 questions.',
+            score=100
+        ),
+        BadgeDetail(
+            name='Bon il y a des limites quand même....',
+            description='Poser 50 questions.',
+            score=200
+        ),
+
+    level_thresholds = [
+            1,
+            20,
+            50,
+        ]
+    ]
+    events = [
+        'on_question_ask',
+    ]
+
+
+    def thresholded_value(self, user):
+            #TODO
+            return 0
+
+
+class AnswerQuestionBadge(ThresholdedBadge):
+    slug = 'question-answer'
+    levels = [
+        BadgeDetail(
+            name='Le savoir c\'est comme la confiture...',
+            description='Répondre à une question',
+            score=20
+        ),
+        BadgeDetail(
+            name='...moins on en a...',
+            description='Répondre à 20 questions.',
+            score=100
+        ),
+        BadgeDetail(
+            name='...plus on l\'étale.',
+            description='Répondre à 50 questions.',
+            score=200
+        ),
+
+    level_thresholds = [
+            1,
+            20,
+            50,
+        ]
+    ]
+    events = [
+        'on_question_answer',
+    ]
+
+    def thresholded_value(self, user):
+            #TODO
+            return 0
---
+++ open_data/user/badges.py
@@ -0,0 +1,176 @@
+from abc import ABCMeta
+
+from badge.base import Badge, BadgeDetail, BadgeAwarded
+from badge.stereotypes import ThresholdedBadge, OnceBadge
+
+
+class UserConnection(OnceBadge):
+    slug = 'user-connection'
+    levels = [
+        BadgeDetail(
+            name='Vous, ici?',
+            description='Se conecter 10 jours d\'affilé'
+            score=20
+        ),
+        BadgeDetail(
+            name='Faites comme chez vous',
+            description='Se conecter 50 jours d\'affilé'
+            score=100
+                ),
+    ]
+
+    level_thresholds = [
+            10,
+            50,
+        ]
+    events = [
+        'on_user_connection',
+    ]
+
+
+
+class UserTimeConnection(OnceBadge):
+    slug = 'user-time-connection'
+    levels = [
+        BadgeDetail(
+            name='Une petite pause ?',
+            description='Rester connecté 30 minutes d\'affilé'
+            score=20
+        ),
+    ]
+    events = [
+        'on_user_time_connection',
+    ]
+
+
+
+class UserDayConnection(OnceBadge):
+    slug = 'user-day-connection'
+    levels = [
+        BadgeDetail(
+            name='Une pomme chaque matin éloigne le médecin',
+            description='Se connecter quotidiennement'
+            score=10
+        ),
+    ]
+    events = [
+        'on_user_day_connection',
+    ]
+
+
+
+class UploadProfilePicture(OnceBadge):
+    slug = 'upload-profile-picture'
+    levels = [
+        BadgeDetail(
+            name='L\'habit ne fait pas le moine',
+            description='Importer une photo de profil'
+            score=20
+        ),
+    ]
+
+    events = [
+        'on_user_picture_upload',
+    ]
+
+
+class ModifyProfilName(OnceBadge):
+    slug = 'modify-profil-name'
+    levels = [
+        BadgeDetail(
+            name='Say my name',
+            description='Changer de nom de profil'
+            score=20
+        ),
+    ]
+
+    events = [
+        'on_user_profil_name',
+    ]
+
+
+class AddBiography(OnceBadge):
+    slug = 'add-biograpy'
+    levels = [
+        BadgeDetail(
+            name='Dis moi qui tu es, je saurai qui tu es.',
+            description='Ajouter une biographie'
+            score=20
+        ),
+    ]
+
+    events = [
+        'on_user_profil_bio',
+    ]
+
+
+#scoreboard
+
+class WinnerBadge(OnceBadge):
+    slug = 'be-the-best'
+    levels = [
+        BadgeDetail(
+            name='Veni vidi vici',
+            description='Être le premier du tableau des scores'
+            score=200
+        ),
+    ]
+
+    events = [
+        'be_the_best',
+    ]
+
+
+class PantheonBadge(OnceBadge):
+    slug = 'pantheon'
+    levels = [
+        BadgeDetail(
+            name='Bienvenue, au pantheon des héros',
+            description='Être dans les 20 premiers du tableau des scores et y rester 5 jours'
+            score=200
+        ),
+    ]
+
+    events = [
+        'stay_in_the_light',
+    ]
+
+
+class BestWinnerBadge(OnceBadge):
+    slug = 'best-winner'
+    levels = [
+        BadgeDetail(
+            name='Tout le monde veut prendre sa place',
+            description='Être  premier du tableau des scores et y rester 24 heures'
+            score=200
+        ),
+    ]
+
+    events = [
+        'stay_in_the_light',
+    ]
+
+
+
+class PokeBadge(OnceBadge):
+    slug = 'pokebadge'
+    levels = [
+        BadgeDetail(
+            name='Collectionneur',
+            description='Gagner 5 badges'
+            score=200
+        ),
+        BadgeDetail(
+            name='Attraper les tous',
+            description='Gagner tous les badges'
+            score=200
+        ),
+    ]
+
+     level_thresholds = [
+        5,
+        20,
+     ]
+    events = [
+        'catch_them_all',
+    ]
