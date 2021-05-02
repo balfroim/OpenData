55a17fb7502983e35c5b8b6360be36a3aa564d16
Pierre Luycx <pierre.luycx@gmail.com>
2021-05-02 14:37:10 +0200

    Add pagination on search page

--- open_data/dataset/templatetags/dataset_tags.py
+++ open_data/dataset/templatetags/dataset_tags.py
@@ -8,3 +8,9 @@ register = template.Library()
 @register.simple_tag
 def get_themes():
     return Theme.get_displayed()
+
+
+@register.filter
+def paginate(sequence, group_size):
+    for i in range(0, len(sequence), group_size):
+        yield sequence[i:i+group_size]
