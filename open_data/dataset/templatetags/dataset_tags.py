9bb34bb4da951fdff02816caa755cddd3a38f92e
Pierre Luycx <pierre.luycx@gmail.com>
2021-05-02 18:06:04 +0200

    Refactor recommendation modal

--- open_data/dataset/templatetags/dataset_tags.py
+++ open_data/dataset/templatetags/dataset_tags.py
@@ -10,6 +10,11 @@ def get_themes():
     return Theme.get_displayed()
 
 
+@register.inclusion_tag('tags/show_datasets.html')
+def show_datasets(datasets):
+    return {'datasets': datasets}
+
+
 @register.filter
 def paginate(sequence, group_size):
     for i in range(0, len(sequence), group_size):
